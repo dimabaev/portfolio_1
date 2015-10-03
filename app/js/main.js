6607530cd2ae584d78663e97a8cbf198efb78e9a
dimabaev <welheor666@gmail.com>
2015-10-04 02:56:42 +0300

    commit12345

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,16 @@ $(function () {
 	});
 });
 
- // $( '.navigation_item' ).click(function() {
- // 	$(this).slideUp();
- // });
+ $(document).ready(function(){
+        //Скрыть PopUp при загрузке страницы    
+        PopUpHide();
+    });
+    //Функция отображения PopUp
+    function PopUpShow(){
+        $("#popup1").show();
+    }
+    //Функция скрытия PopUp
+    function PopUpHide(){
+        $("#popup1").hide();
+    }
 
